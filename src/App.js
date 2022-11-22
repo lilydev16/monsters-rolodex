c85d5e8817804e80820201c93149068b6cac449a
lswatson16 <lauralynw16@gmail.com>
2022-11-21 20:21:40 -0500

    Implement a filtered search feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
     console.log('constructor')
   }
@@ -24,11 +25,31 @@ class App extends Component {
       ))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
   render() {
     console.log('render')
+
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
     return (
       <div className='App'>
-        {this.state.monsters.map(monster => {
+        <input 
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map(monster => {
           return (
             <div key={monster.id}><h1>{monster.name}</h1>
             </div>
